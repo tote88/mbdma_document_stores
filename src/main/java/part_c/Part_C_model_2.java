cb91fd78fca26d329b0635df363d1578ec136ead
Jordi Casanovas <jordi.casanovas@fib.upc.edu>
2018-04-06 19:27:57 +0200

    add model2

--- src/main/java/part_c/Part_C_model_2.java
+++ src/main/java/part_c/Part_C_model_2.java
@@ -1,8 +1,59 @@
 package part_c;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import io.codearte.jfairy.Fairy;
+import io.codearte.jfairy.producer.company.Company;
+import io.codearte.jfairy.producer.person.Person;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
 public class Part_C_model_2 {
 
 	public static void dataGenerator(int N) {
-		
+		Fairy fairy = Fairy.create();
+
+		MongoClient client = new MongoClient();
+		MongoDatabase database = client.getDatabase("model2");
+		MongoCollection<Document> personCollection = database.getCollection("person");
+
+		for (int i = 0; i < N; ++i) {
+			// COMPANY:
+			Company company = fairy.company();
+
+			Document companyDocument = new Document();
+			companyDocument.put("name", company.getName());
+			companyDocument.put("domain", company.getDomain());
+			companyDocument.put("email", company.getEmail());
+			companyDocument.put("vatIdentificationNumber", company.getVatIdentificationNumber());
+
+			//System.out.println(id.toString());
+			// PERSONA:
+			Person person = fairy.person();
+
+			Document personDocument = new Document();
+			personDocument.put("firstName", person.getFirstName());
+			personDocument.put("middleName", person.getMiddleName());
+			personDocument.put("lastName", person.getLastName());
+			personDocument.put("email", person.getEmail());
+			personDocument.put("username", person.getUsername());
+			personDocument.put("passportNumber", person.getPassportNumber());
+			personDocument.put("address", person.getAddress().toString());
+			personDocument.put("password", person.getPassword());
+			personDocument.put("sex", person.getSex().toString());
+			personDocument.put("telephoneNumber", person.getTelephoneNumber());
+			personDocument.put("dateOfBirth", person.getDateOfBirth().toString());
+			personDocument.put("age", person.getAge());
+			personDocument.put("nationalIdentityCardNumber", person.getNationalIdentityCardNumber());
+			personDocument.put("nationalIdentificationNumber", person.getNationalIdentificationNumber());
+			personDocument.put("company", companyDocument);
+
+			System.out.println(personDocument.toJson());
+
+			personCollection.insertOne(personDocument);
+
+		}
+		client.close();
 	}
 }
