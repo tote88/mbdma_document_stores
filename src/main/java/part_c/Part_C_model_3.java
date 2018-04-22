7677ec441e0f3bf6d1ce51bc04bd67bb410100a1
Jordi Casanovas <tote88@gmail.com>
2018-04-22 22:54:03 +0200

    add queries from Victor for model 3

--- src/main/java/part_c/Part_C_model_3.java
+++ src/main/java/part_c/Part_C_model_3.java
@@ -1,15 +1,19 @@
 package part_c;
 
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import io.codearte.jfairy.Fairy;
 import io.codearte.jfairy.producer.company.Company;
 import io.codearte.jfairy.producer.person.Person;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class Part_C_model_3 {
 
@@ -58,6 +62,65 @@ public class Part_C_model_3 {
 			companyCollection.insertOne(companyDocument);
 
 		}
+		/* QUERY 1: For each person, its full name and company */
+		long startTime = System.currentTimeMillis(); // Get time at the start of the query
+		FindIterable<Document> results = companyCollection.find();
+		for (Document company : results) {
+			ArrayList<Document> persons = (ArrayList) company.get("persons");
+
+			
+			for (Document person : persons) {
+//				System.out.println(person.getString("firstName") + " " + person.getString("middleName") + " " +
+//					person.getString("lastName") + " works at " + company.get("name"));
+			}
+		}
+		long queryTime1 = System.currentTimeMillis() - startTime; // Measure query execution time
+		
+		/* QUERY 2: For each company, name and number of employees*/
+		startTime = System.currentTimeMillis(); // Get time at the start of the query
+		results = companyCollection.find();
+		for (Document company : results) {
+			ArrayList<Document> persons = (ArrayList) company.get("persons");
+
+//			System.out.println("At " + company.get("name") + " work " + persons.size() + " people.");
+		}
+		long queryTime2 = System.currentTimeMillis() - startTime; // Measure query execution time
+		
+		/* QUERY 3 For each person born before 1988, update age to 30*/
+		startTime = System.currentTimeMillis(); // Get time at the start of the query
+		results = companyCollection.find();
+		
+		Document update = new Document();
+		Document ageDoc = new Document();
+		ageDoc.put("persons.$[].age", 30);
+		update.put("$set", ageDoc);
+		
+		Document query = new Document();
+		Document lessThan1988 = new Document();
+		lessThan1988.put("$lt", "1988-01-01");
+		query.put("persons.dateOfBirth", lessThan1988);
+		companyCollection.updateMany(query, update);
+		long queryTime3 = System.currentTimeMillis() - startTime; // Measure query execution time
+
+		/* QUERY 4 update company name to include Company word*/
+		startTime = System.currentTimeMillis(); // Get time at the start of the query
+
+		results = companyCollection.find();
+		for (Document doc : results) {
+			ObjectId companyId = doc.getObjectId("_id");
+			String companyName = doc.getString("name").concat(" Company");
+			companyCollection.updateOne(
+					new Document("_id", companyId),
+					new Document("$set", new Document("name", companyName))
+			);
+		}
+		long queryTime4 = System.currentTimeMillis() - startTime; // Measure query execution time
+
+
 		client.close();
+		System.out.println("\nTIME USED FOR QUERY 1: " + queryTime1);
+		System.out.println("\nTIME USED FOR QUERY 2: " + queryTime2);
+		System.out.println("\nTIME USED FOR QUERY 3: " + queryTime3);
+		System.out.println("\nTIME USED FOR QUERY 4: " + queryTime4);
 	}
 }
